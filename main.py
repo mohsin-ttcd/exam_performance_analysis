0af0bea5733aae33ca19268870cecef3075cfad5
agentMohsin <mohsin.ttcdl@gmail.com>
2026-05-25 21:34:44 +0600

    Initial commit with project and gitignore

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# Import pandas and saved give it a nick name pd
+import pandas as pd
+
+# Import full table as DataFrame
+df = pd.read_csv("Student-Data/StudentsPerformance.csv")
+
+# print first 5line of Data Set
+print(df.head())
+
+# 1. What are the average marks in Math, Reading, and Writing?
+
+
+
