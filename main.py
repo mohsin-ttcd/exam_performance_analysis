b88b7779e9482f0ef62103fde31ea9fc3a7932a0
agentMohsin <mohsin.ttcdl@gmail.com>
2026-05-26 22:38:13 +0600

    added chain method instead of sequentially mutating a dataframe line-by-line

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ df = pd.read_csv("Student-Data/StudentsPerformance.csv")
 
 # 1. What are the average marks in Math, Reading, and Writing?
 
-# convert string into integer inside math score
-df["math score"] = pd.to_numeric(df["math score"], errors="coerce")
-df["reading score"] = pd.to_numeric(df["reading score"], errors="coerce")
-df["writing score"] = pd.to_numeric(df["writing score"], errors="coerce")
-
-# cleaning unexpected string
-df["math score"] = df["math score"].fillna(0)
-df["reading score"] = df["reading score"].fillna(0)
-df["writing score"] = df["writing score"].fillna(0)
+# convert string into integer inside score column
+df = df.assign(
+    **{
+        "math score" : pd.to_numeric(df["math score"], errors='coerce').dropna(),
+        "reading score" : pd.to_numeric(df["reading score"], errors='coerce').dropna(),
+        "writing score" : pd.to_numeric(df["writing score"], errors='coerce').dropna()
+    }
+)
 
 # average marks in Math, Reading, and Writing
 print(f"Average marks in Math: {df['math score'].mean():.2f}")
